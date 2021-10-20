1412c133a2d11f6bb9ed3419450363a941f6d2fe
Alfredo Alejandro Villegas <xillegas@gmail.com>
2021-10-20 16:04:13 -0400

    added task model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ----Read----
+  # Get all task
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # Get one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # ----Create----
+  # Show form
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+
+  #----Update----
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'restaurant#update'
+
+  # ----destroy----
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
